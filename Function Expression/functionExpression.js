55f821473656371c4efdc2c0999d79d42209a592
Bijay B.K <bijaybk307@gmail.com>
2024-07-10 03:10:08 +0545

    first commit

---
+++ Function Expression/functionExpression.js
@@ -0,0 +1,5 @@
+// Function Expressions
+// A JavaScript function can also be defined using an expression.
+
+const x = function (a, b) {return a * b};
+document.getElementById("functionExpression").innerHTML = x(4, 3);
